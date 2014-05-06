31c4d5ac5196cde5d8126fa32f708fd61c09734e
Corbin Hughes <hughes.corbin@gmail.com>
2014-05-05 19:12:45 -0500

    Updated ignore files and made API give out a confirmation

--- web/public/api.php
+++ web/public/api.php
@@ -9,6 +9,7 @@ if (!empty($_REQUEST['key']) && $_REQUEST['key'] === 'hai') {
 	if(preg_match('/^[a-z]+$/', $host)) {
 		$dr = new DirectoryRepository(REPO_PATH);
 		$dr->storeServerRecord($host, $_POST);
+		echo 'ok';
 	}
 } else {
 	echo 'Invalid key';
