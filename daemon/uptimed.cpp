31e49d2bf88abbf8a0ff8b7831abd4477553ee8a
Corbin Hughes <hughes.corbin@gmail.com>
2014-09-08 22:31:22 -0500

    Refactoring

--- daemon/uptimed.cpp
+++ daemon/uptimed.cpp
@@ -10,6 +10,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <functional>
 
 #include <unistd.h>
 #include <getopt.h>
@@ -25,13 +26,13 @@ const char usageMessage[] =
 
 const char versionMessage[] = "uptimed version 0.2 by Corbin Hughes (http://github.com/nibroc/)\n";
 
-void do_or_die(enum procparse_result result, const char* msg) {
+static void do_or_die(enum procparse_result result, const char* msg) {
 	if (!result) { return; }
 	std::fprintf(stderr, "Error: %s -- %s\n", msg, procparse_strerr(result));
 	exit(EXIT_FAILURE);
 }
 
-void print_uptime_info(FILE* stream, const char* host, const struct ProcParseUptime* up,
+static void print_uptime_info(FILE* stream, const char* host, const struct ProcParseUptime* up,
 						const struct ProcParseMemInfo* mem, const struct ProcParseLoadAvg* avg) {
 	std::fprintf(stream, "%s %.0f/%.0f %d/%d %.2f %.2f %.2f %d/%d\n",
 		host, up->total, up->idle,
@@ -44,11 +45,11 @@ void print_uptime_info(FILE* stream, const char* host, const struct ProcParseUpt
 typedef struct ProgramOptions {
 	int repetitions;
 	int repeat_interval;
-	int silent;
+	bool silent;
 	char* url;
 } ProgramOptions;
 
-ProgramOptions parse_args(int argc, char** argv) {
+static ProgramOptions parse_args(int argc, char** argv) {
 	// By default, repeat forever, once every 3 minutes
 	ProgramOptions opts = {-1, 180};
 
@@ -65,7 +66,7 @@ ProgramOptions parse_args(int argc, char** argv) {
 				opts.repeat_interval = atoi(optarg);
 				break;
 			case 's':
-				opts.silent = 1;
+				opts.silent = true;
 				break;
 			case 'v':
 				std::fputs(versionMessage, stderr);
@@ -86,44 +87,59 @@ ProgramOptions parse_args(int argc, char** argv) {
 	return opts;
 }
 
+static timespec get_time() {
+	timespec now;
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	return now;
+}
+
+static double elapsed_seconds(const timespec& start, const timespec& end) {
+	double seconds = end.tv_sec - start.tv_sec;
+	double nanoseconds = end.tv_nsec - start.tv_nsec;
+	return seconds + nanoseconds / 1.0e9;
+}
+
+template<typename Func>
+static void repeat(int times, double interval, Func func) {
+	do {
+		timespec start_time = get_time();
+		func();
+		if (times >= 0 && --times == 0) {
+			break;
+		} else {
+			double elapsed_secs = elapsed_seconds(start_time, get_time());
+			double sleep_us = (interval - elapsed_secs) * 1.0e6;
+			if (sleep_us > 0) {
+				usleep(sleep_us);
+			}
+		}
+	} while (1);
+}
+
+void notify_server(Notifier& notify, const ProgramOptions& opts) {
+	char host[256];
+	do_or_die(procparse_hostname(host, sizeof(host)), "Could not read hostname");
+	struct ProcParseUptime up;
+	do_or_die(procparse_uptime(&up), "Could not get uptime");
+	struct ProcParseMemInfo mem;
+	do_or_die(procparse_meminfo(&mem), "Could not get meminfo");
+	struct ProcParseLoadAvg avg;
+	do_or_die(procparse_loadavg(&avg), "could not get loadavg");
+
+	if (!opts.silent) {
+		print_uptime_info(stdout, host, &up, &mem, &avg);
+	}
+	auto result = notify.send(host, &avg, &mem, &up);
+	if (result.is_failure()) {
+		std::fprintf(stderr, "%s\n", result.error_message().c_str());
+	}
+}
+
 int main(int argc, char** argv) {
 	ProgramOptions opts = parse_args(argc, argv);
 
 	Notifier notify(opts.url);
 
-	do {
-		char host[256];
-		do_or_die(procparse_hostname(host, sizeof(host)), "Could not read hostname");
-		struct ProcParseUptime up;
-		do_or_die(procparse_uptime(&up), "Could not get uptime");
-		struct ProcParseMemInfo mem;
-		do_or_die(procparse_meminfo(&mem), "Could not get meminfo");
-		struct ProcParseLoadAvg avg;
-		do_or_die(procparse_loadavg(&avg), "could not get loadavg");
-
-		if (!opts.silent) {
-			print_uptime_info(stdout, host, &up, &mem, &avg);
-		}
-
-		struct timespec start, end;
-
-		clock_gettime(CLOCK_MONOTONIC, &start);
-
-		auto result = notify.send(host, &avg, &mem, &up);
-		if (result.is_failure()) {
-			std::fprintf(stderr, "%s\n", result.error_message().c_str());
-		}
-		clock_gettime(CLOCK_MONOTONIC, &end);
-
-		if (opts.repetitions != -1 && --opts.repetitions == 0) {
-			break;
-		}
-
-		double tdiff = ((double) (end.tv_sec - start.tv_sec)) + ( ((double)(end.tv_nsec - start.tv_nsec)) / 1000000000);
-		if (opts.repeat_interval > tdiff) {
-			usleep((opts.repeat_interval - tdiff) * 1000000 );
-		}
-	} while(1);
-
-	return 0;
+	repeat(opts.repetitions, opts.repeat_interval,
+		std::bind(notify_server, std::ref(notify), opts));
 }
